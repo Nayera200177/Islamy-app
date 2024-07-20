b76739983e0c617c40aa95e930de878081bc4a03
PC <Nayera200177@outlook.com>
2024-07-20 09:37:57 +0300

    project images added. bottom Navigation Bar created and work.

---
+++ lib/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class AppColors{
+  static const Color blackColor = Color(0xff242424);
+  static const Color primaryLightColor = Color(0xffB7935F);
+  static const Color whightColor = Color(0xffffffff);
+}
